05c595794fef5b446790eb28f251b345d9702299
Denise Salazar <denise.salazar.1210@gmail.com>
2023-02-07 16:16:24 -0800

    get stock and show

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const express = require('express'); // pulls express into app
 const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
+const axios = require('axios')
+const bodyParser = require('body-parser');
+
 
 // pushing to a web host, will pick up PORT env var
 // locally, port is 5000
 const PORT = process.env.PORT || 5000;
+
+// use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+function call_api(finishedAPI, stockTicker) {
+	const token = process.env.IEX_CLOUD_TOKEN
+	const url = "https://cloud.iexapis.com/stable/stock/" + stockTicker + "/quote?token=" + token
+	console.log("GET " + url);
+	axios.get(url)
+	.then(res => {
+		console.log("Status Code: ", res.status);
+		finishedAPI(res.data);
+	})
+	.catch(err => {
+		console.log("Error: ", err.message);
+		finishedAPI(err.message);
+	});
+}
 
 // Set Handlebars middleware
 app.engine('handlebars', exphbs.engine());
@@ -14,11 +40,23 @@ app.set('views', './views');
 
 const otherStuff = "other stuff";
 
+
 // set handlebar routes
 app.get('/', (req, res) => {
-    res.render('home', {
-    	stuff: otherStuff
-    });
+	call_api(function(doneAPI) {
+		res.render('home', {
+    		stock: doneAPI
+    	});
+	}, "fb");    
+});
+
+app.post('/', (req, res) => {
+	// posted_stuff = req.body.stock_ticker;
+	call_api(function(doneAPI) {
+		res.render('home', {
+			stock: doneAPI
+		});
+	}, req.body.stock_ticker);
 });
 
 app.get('/about.html', (req, res) => {
